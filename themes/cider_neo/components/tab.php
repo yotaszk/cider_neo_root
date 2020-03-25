2b91216e71437fa32fb837df0e755e9fd191db78
Yota Suzuki <yotaszk@Yota-MacBook-Pro.local>
2020-03-25 09:50:38 +0900

    separate wp and themes repo

--- themes/cider_neo/components/tab.php
+++
@@ -1,8 +0,0 @@
-<ul class="Tab">
-  <li><a class="Tab-Link" href="/mac">Mac</a></li>
-  <li><a class="Tab-Link" href="/mac/macbookpro">MacBook</a></li>
-  <li><a class="Tab-Link" href="/ipad">iPad</a></li>
-  <li><a class="Tab-Link" href="/ipad/ipadpro">iPad Pro</a></li>
-  <li><a class="Tab-Link" href="/iphone">iPhone</a></li>
-  <li><a class="Tab-Link" href="/apple-watch">Watch</a></li>
-</ul>
